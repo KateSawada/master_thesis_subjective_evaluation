9d9bd3febe46cfc9a6d239de1f1769c647e74a83
KateSawada <nuksawada@gmail.com>
2023-01-08 22:14:39 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,72 @@
+/**
+ * [x]「次へ」　を押すと現在の .page を非表示にして，次の .page を表示
+ * * 最初に全ての .page を配列で取得 -> 現在のindexを保持 -> 一個ずつずらしていく
+ *
+ * [x] 最後は .page ではなく， #final-page に飛ばす
+ *
+ * [x] 全ての .page の表示時に 「次へ」を表示
+ *
+ * [ ] canGoNextPage の処理
+ */
+
+let canGoNextPage = true;
+let pageCount = 0;
+let currentPageIndex = -1;
+let pages = [];
+
+function showElement(elem) {
+    elem.style.display = "block";
+}
+
+function hideElement(elem) {
+    elem.style.display = "none";
+}
+
+function onNextButtonClick() {
+    console.log("currentPageIndex(before): " + currentPageIndex);
+    if (canGoNextPage) {
+        if (pageCount == currentPageIndex + 1) {
+            // final-page へ飛ばす
+            hideElement(pages[currentPageIndex]);
+            showElement(document.getElementById("final-page"));
+        } else {
+            // page
+            hideElement(pages[currentPageIndex]);
+            currentPageIndex += 1;
+            showElement(pages[currentPageIndex]);
+        }
+    }
+    console.log("currentPageIndex(after): " + currentPageIndex);
+}
+
+function getNextButton() {
+    let btn = document.createElement('button');
+    btn.onclick = onNextButtonClick;
+    btn.innerText = "次へ";
+    return btn;
+}
+
+function setup(){
+    window.addEventListener('DOMContentLoaded', (event) => {
+        pages = document.getElementsByClassName("page");
+        console.log(pages);
+        pageCount = pages.length;
+        console.log(pageCount);
+        for (let i = 0; i < pages.length; i++) {
+            console.log(pages[i]);
+            pages[i].style = "display: none;";
+            pages[i].appendChild(getNextButton());
+        }
+        showElement(pages[0]);
+
+        currentPageIndex = 0;
+    });
+}
+
+
+function main() {
+    setup();
+}
+
+
+main();
